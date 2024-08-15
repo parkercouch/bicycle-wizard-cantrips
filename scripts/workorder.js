b92bfd8988e95de80da100d64914cc7b3bc0c89a
parker couch <code@parkercouch.dev>
2024-08-15 16:35:34 -0700

    Iterate on simple ui design
    adding more to top bar to remove from simple ui

--- scripts/workorder.js
+++ scripts/workorder.js
@@ -4,33 +4,23 @@
     return;
   }
 
-  inject_toggle_internal_notes_button();
-  inject_simple_ui_toggle_button();
+  inject_ui_toggle_buttons();
   format_notes_boxes();
   copy_workorder_info_to_topbar();
 })();
 
-function inject_toggle_internal_notes_button() {
-  const internal_notes_container = document.querySelector("#workorder_status_wrapper > div.view_field_box > div:nth-child(2)");
-  const workorder_dates_row = document.querySelector("#workorder_status_wrapper > fieldset.workorder-dates");
-
-  const toggle_internal_notes_button = document.createElement("ol");
-  toggle_internal_notes_button.innerHTML = `
-    <button type="button" style="padding: 0 0.5rem; height: 1.5rem; margin-top: 1.25rem;">
-      <span style="display: block;">Toggle Internal Notes</span>
-    </button>
-  `;
-  toggle_internal_notes_button.onclick = function () {
-    toggle_display(internal_notes_container);
-  };
-
-  workorder_dates_row.append(toggle_internal_notes_button);
-}
-
+/**
+  * Move notes into a single column and make the default size larger
+  */
 function format_notes_boxes() {
   const notes_container = document.querySelector("div.view_field_box:has(label[for=note])");
   const notes_boxes = document.querySelectorAll("#workorder_status_wrapper > div.view_field_box > *");
   const note_text_area = document.querySelector("#noteTextArea");
+  const hook_in = document.querySelector("#hookInInputField");
+  const hook_out = document.querySelector("#hookOutInputField");
+
+  hook_in.style.setProperty("width", "8rem");
+  hook_out.style.setProperty("width", "8rem");
 
   notes_container.style["flex-flow"] = "column";
   for (let i = 0; i < notes_boxes.length; i++) {
@@ -38,8 +28,12 @@ function format_notes_boxes() {
     notes_boxes[i].style.setProperty("width", "-webkit-fill-available");
   }
   note_text_area.style.setProperty("height", "15rem");
+  note_text_area.style.setProperty("font-size", "medium");
 }
 
+/**
+  * Copy verbose presentation of needed data into condensed form on topbar
+  */
 function copy_workorder_info_to_topbar() {
   const topbar_breadcrumbs = document.querySelector("#wrapper > div.js-topbar > div > div.cr-topbar__breadcrumbs");
   const topbar = document.querySelector("#wrapper > div.js-topbar > div");
@@ -48,15 +42,32 @@ function copy_workorder_info_to_topbar() {
   const bike_color = document.querySelector("#serial_color").value;
   const bike_size = document.querySelector("#serial_size").value;
 
+  topbar_breadcrumbs.style.setProperty("padding-right", "1rem");
+
   const topbar_info_container = topbar_breadcrumbs.cloneNode();
   topbar_info_container.innerHTML = `
     <p>${bike_description}: ${bike_color}, ${bike_size}</p>
   `;
 
+  const due_date = document.querySelector("#workorder_edit_time_out_field_date").value;
+  const hook_in = document.querySelector("#hookInInputField").value;
+  const hook_out = document.querySelector("#hookOutInputField").value;
+
+  const topbar_dates_container = topbar_breadcrumbs.cloneNode();
+  topbar_dates_container.style.setProperty("flex-flow", "column");
+  topbar_dates_container.innerHTML = `
+    <div style="margin: 0; padding: 0;">DUE: ${due_date}</div>
+    <div style="margin: 0; padding: 0;">In: ${hook_in}, Out: ${hook_out}</div>
+  `;
+
   topbar.appendChild(topbar_info_container);
+  topbar.appendChild(topbar_dates_container);
 }
 
-function inject_simple_ui_toggle_button() {
+/**
+  * Add buttons that modify the ui
+  */
+function inject_ui_toggle_buttons() {
   const topbar = document.querySelector("#wrapper > div.js-topbar > div");
   const topbar_toggler = document.querySelector("#wrapper > div.js-topbar > div > div:nth-child(1)");
   const topbar_breadcrumbs = document.querySelector("#wrapper > div.js-topbar > div > div.cr-topbar__breadcrumbs");
@@ -65,23 +76,43 @@ function inject_simple_ui_toggle_button() {
   const bike_description_row = document.querySelector("#workorder_status_wrapper > fieldset:nth-child(3)");
   const employee_row = document.querySelector("#workorder_status_wrapper > fieldset.workorder-employee");
   const button_row = document.querySelector("#view > div > div.functions");
+  const workorder_dates_row = document.querySelector("#workorder_status_wrapper > fieldset.workorder-dates");
+  const internal_notes_container = document.querySelector("#workorder_status_wrapper > div.view_field_box > div:nth-child(2)");
 
-  const toggle_simple_ui_button = topbar_toggler.cloneNode();
-  toggle_simple_ui_button.innerHTML = `
-    <button type="button" style="">
+  const toggle_buttons = topbar_toggler.cloneNode();
+  toggle_buttons.style.setProperty("width", "2rem");
+  toggle_buttons.classList.replace("cr-topbar__toggler", "ui_toggler");
+  toggle_buttons.innerHTML = `
+    <button type="button" style="color: grey; font-size: medium">
       <span style="display: block;">UI</span>
     </button>
+    <button type="button" style="color: grey; font-size: medium">
+      <span style="display: block;">IN</span>
+    </button>
   `;
-  toggle_simple_ui_button.onclick = function () {
+
+  const ui_button = toggle_buttons.childNodes[1];
+  const internal_notes_button = toggle_buttons.childNodes[3];
+
+  ui_button.onclick = function () {
+    toggle_color(ui_button);
     toggle_display(customer_info);
     toggle_display(bike_description_row);
     toggle_display(employee_row);
     toggle_display(button_row);
+    toggle_display(workorder_dates_row);
+  };
+  internal_notes_button.onclick = function () {
+    toggle_color(internal_notes_button);
+    toggle_display(internal_notes_container);
   };
 
-  topbar.insertBefore(toggle_simple_ui_button, topbar_breadcrumbs);
+  topbar.insertBefore(toggle_buttons, topbar_breadcrumbs);
 }
 
 function toggle_display(element) {
   element.style.display = element.style.display === "none" ? null : "none";
+}
+function toggle_color(element) {
+  element.style.color = element.style.color === "grey" ? "black" : "grey";
 }
