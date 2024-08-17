b4270e27759b22d444e8c086c4068c50033acc45
parker couch <code@parkercouch.dev>
2024-08-17 12:31:59 -0700

    fix workorder ui simplify action
    moved data to an element that isn't sticky. the topbar is saved across
    pages so would have to start syncing data. Instead just moved to a
    toggle that scrapes the data everytime it is toggled.

--- scripts/workorder.js
+++ scripts/workorder.js
@@ -6,7 +6,6 @@
 
   inject_ui_toggle_buttons();
   format_notes_boxes();
-  copy_workorder_info_to_topbar();
 })();
 
 /**
@@ -32,47 +31,57 @@ function format_notes_boxes() {
 }
 
 /**
-  * Copy verbose presentation of needed data into condensed form on topbar
+  * Copy verbose presentation of needed data into condensed form on customer row
   */
-function copy_workorder_info_to_topbar() {
-  const topbar_breadcrumbs = document.querySelector("#wrapper > div.js-topbar > div > div.cr-topbar__breadcrumbs");
-  const topbar = document.querySelector("#wrapper > div.js-topbar > div");
+function copy_workorder_info_to_customer_row() {
+  const workorder_data = get_workorder_data();
+  const functions_row = document.querySelector("#view > div > div.functions");
+  const customer_name = document.querySelector("hgroup.workorder-customer-basics");
 
-  const bike_description = document.querySelector("#serial_description").value;
-  const bike_color = document.querySelector("#serial_color").value;
-  const bike_size = document.querySelector("#serial_size").value;
 
-  topbar_breadcrumbs.style.setProperty("padding-right", "1rem");
+  const info_bar = functions_row.cloneNode();
+  info_bar.removeAttribute("class");
+  customer_name.after(info_bar);
 
-  const topbar_info_container = topbar_breadcrumbs.cloneNode();
-  topbar_info_container.innerHTML = `
-    <p>${bike_description}: ${bike_color}, ${bike_size}</p>
+  info_bar.outerHTML = `
+    <div style="display: flex; flex-flow: row" id="workorder-info-bar">
+      <div style="margin: 0.5rem; padding: 0;">
+        <h3 style="margin: 0; padding: 0;">${workorder_data.bike_description}: ${workorder_data.bike_color}, ${workorder_data.bike_size}</h3>
+        <h4 style="margin: 0; padding: 0;">DUE: ${workorder_data.due_date}</h4>
+      </div>
+      <div style="margin: 0.5rem; padding: 0;">
+        <h3 style="margin: 0; padding: 0;">Hook In: ${workorder_data.hook_in}</h3>
+        <h3 style="margin: 0; padding: 0;">Hook Out: ${workorder_data.hook_out}</h3>
+      </div>
+    </div>
   `;
+}
 
-  const due_date = document.querySelector("#workorder_edit_time_out_field_date").value;
-  const hook_in = document.querySelector("#hookInInputField").value;
-  const hook_out = document.querySelector("#hookOutInputField").value;
-
-  const topbar_dates_container = topbar_breadcrumbs.cloneNode();
-  topbar_dates_container.style.setProperty("flex-flow", "column");
-  topbar_dates_container.innerHTML = `
-    <div style="margin: 0; padding: 0;">DUE: ${due_date}</div>
-    <div style="margin: 0; padding: 0;">In: ${hook_in}, Out: ${hook_out}</div>
-  `;
-
-  topbar.appendChild(topbar_info_container);
-  topbar.appendChild(topbar_dates_container);
+/**
+  * scrape data from workorder
+  */
+function get_workorder_data() {
+  return {
+    bike_description: document.querySelector("#serial_description").value,
+    bike_color: document.querySelector("#serial_color").value,
+    bike_size: document.querySelector("#serial_size").value,
+    due_date: document.querySelector("#workorder_edit_time_out_field_date").value,
+    hook_in: document.querySelector("#hookInInputField").value,
+    hook_out: document.querySelector("#hookOutInputField").value,
+  };
 }
 
 /**
   * Add buttons that modify the ui
   */
 function inject_ui_toggle_buttons() {
-  const topbar = document.querySelector("#wrapper > div.js-topbar > div");
   const topbar_toggler = document.querySelector("#wrapper > div.js-topbar > div > div:nth-child(1)");
-  const topbar_breadcrumbs = document.querySelector("#wrapper > div.js-topbar > div > div.cr-topbar__breadcrumbs");
+  const topbar = document.querySelector("div.js-topbar");
+  const page_wrapper = document.querySelector("#wrapper");
 
   const customer_info = document.querySelector("div.workorder-customer");
+  const customer_buttons = document.querySelector("div.workorder-customer-buttons");
+  const customer_contact = document.querySelector("div.workorder-customer-contact");
   const bike_description_row = document.querySelector("#workorder_status_wrapper > fieldset:nth-child(3)");
   const employee_row = document.querySelector("#workorder_status_wrapper > fieldset.workorder-employee");
   const button_row = document.querySelector("#view > div > div.functions");
@@ -81,6 +90,7 @@ function inject_ui_toggle_buttons() {
 
   const toggle_buttons = topbar_toggler.cloneNode();
   toggle_buttons.style.setProperty("width", "2rem");
+  toggle_buttons.style.setProperty("margin", "5px");
   toggle_buttons.classList.replace("cr-topbar__toggler", "ui_toggler");
   toggle_buttons.innerHTML = `
     <button type="button" style="color: grey; font-size: medium">
@@ -96,18 +106,31 @@ function inject_ui_toggle_buttons() {
 
   ui_button.onclick = function () {
     toggle_color(ui_button);
-    toggle_display(customer_info);
+    toggle_display(customer_buttons);
+    toggle_display(customer_contact);
     toggle_display(bike_description_row);
     toggle_display(employee_row);
     toggle_display(button_row);
     toggle_display(workorder_dates_row);
+
+    toggle_display(topbar);
+    page_wrapper.style["padding-top"] = page_wrapper.style["padding-top"] === "0px" ? "54px" : "0px";
+
+    if (!!document.querySelector("#workorder-info-bar")) {
+      document.querySelector("#workorder-info-bar").remove();
+    } else {
+      copy_workorder_info_to_customer_row();
+    }
   };
   internal_notes_button.onclick = function () {
     toggle_color(internal_notes_button);
     toggle_display(internal_notes_container);
   };
 
-  topbar.insertBefore(toggle_buttons, topbar_breadcrumbs);
+  customer_info.style.setProperty("display", "flex");
+  customer_info.style.setProperty("padding", "5px");
+
+  customer_buttons.after(toggle_buttons);
 }
 
 function toggle_display(element) {
