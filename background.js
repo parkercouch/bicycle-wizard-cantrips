b102e9af3631d1803527b11171aff3947ee3ab8c
parker couch <code@parkercouch.dev>
2024-08-21 05:22:02 -0700

    update scripts to inject on url change
    since lightspeed is a spa-like the content injection was only happening
    on a hard reload of a page. Changing the injection to an on url change
    event handler improves the consistency of the scripts being injected.

--- background.js
+++ background.js
@@ -6,3 +6,29 @@ chrome.runtime.onInstalled.addListener(() => {
     text: "on",
   });
 });
+
+chrome.tabs.onUpdated.addListener(async function on_url_update(tabId, changeInfo, _tab) {
+  if (!changeInfo.url) {
+    return;
+  }
+
+  if (changeInfo.url.includes("us.merchantos.com/?name=workbench.views")) {
+    await chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        files: ['scripts/workorder.js'],
+      }
+    );
+    return;
+  }
+
+  if (changeInfo.url.includes("us.merchantos.com/?name=workbench.listings.workorders_agenda")) {
+    await chrome.scripting.executeScript(
+      {
+        target: { tabId: tabId },
+        files: ['scripts/schedule.js'],
+      }
+    );
+    return;
+  }
+});
